2382c9517a6a2c3e9576b64c86eebb35a69fc20c
Ben Garside <Ben.Garside@bsigroup.com>
2016-09-05 09:33:56 +0100

    reformat files and tidy up

--- sru_schtasks/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import schtasks
-
-def setup(app):
-    app.router.add_route("POST", "/schtasks/query", schtasks.query)
-    app.router.add_route("POST", "/schtasks/edit", schtasks.edit)
-    app.router.add_route("POST", "/schtasks/control", schtasks.control)
