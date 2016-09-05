4fc9fe94fa0f146ecbc185037ec43470a73f3437
Ben Garside <Ben.Garside@bsigroup.com>
2016-09-05 09:34:17 +0100

    remove packages

--- setup.py
+++ setup.py
@@ -19,20 +19,14 @@ setup(
     author_email="abgarside<at>gmail<dot>com",
     packages=[
         "sru_schtasks",
-        "sru_schtasks.routes",
-        "sru_schtasks.conf",
-        "sru_schtasks.helpers",
-        "sru_schtasks.controllers"
         ],
     include_package_data=True,
     url="http://github.com/ben-garside/sru_schtasks/dist/{}/".format(version),
     description="schtasks SRU package",
     dependency_links=[
-        "https://github.com/ben-garside/schtasks_shim/tarball/master#egg=schtasks_shim-0.1.1'"
+        "https://github.com/ben-garside/schtasks_shim/tarball/master#egg=schtasks_shim-0.1.1"
     ],
     install_requires=[
-        "voluptuous",
-        "aiohttp",
         "schtasks_shim>=0.1.1"
     ],
 )
